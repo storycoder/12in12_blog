a03fe4854199dc1ea812972fb1213f1d37f5d526
Storycoder <joseph.margolis@gmail.com>
2015-10-11 10:14:49 -0400

    broken posts integration test

---
+++ test/integration/posts_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class PostsTest < ActionDispatch::IntegrationTest
+  
+  def set_up
+  	@entry_one = posts(:entry_one)
+  	@entry_two = posts(:entry_two)
+
+  	visit_events_path
+
+  end
+
+test 'Index page shows all posts' do 
+	assert page.has_content?('Posts')
+
+	assert page.has_content?(@entry_one.title)
+	assert page.has_content?(@entry_two.body)
+
+	assert page.has_content?(@entry_one.title)
+	assert page.has_content?(@entry_two.body)
+
+end
+
+test 'Can create posts' do 
+
+	click_link('Create Post')
+
+	fill_in 'Title', with: 'My Blog Entry'
+	fill_in 'Body', with: 'My ipsum is your ipsum'
+
+	click_button 'Create Post'
+
+	assert page.has_content?('My Blog Entry')
+	assert page.has_content?('My ipsum is your ipsum')
+end
+
+end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  
+  test 'has a valid fixture' do 
+  	@post = posts(:entry_one)
+  	assert @post.valid?
+  end
+
   should validate_presence_of(:title)
   should validate_presence_of(:body)
 end
