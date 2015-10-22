e4ce0622b416b41066758e0ece300c5eac5fb5e9
Storycoder <joseph.margolis@gmail.com>
2015-10-21 23:11:44 -0400

    working on integration test that verifies max char ct on posts displayed on index

--- test/integration/posts_test.rb
+++ test/integration/posts_test.rb
@@ -4,13 +4,12 @@ class PostsTest < ActionDispatch::IntegrationTest
   
   def setup
   	@entry_one = posts(:entry_one)
-  	@entry_two = posts(:entry_two)
 
   	visit posts_path
 
   end
 
-  	test 'sidebar shows all content & clicks all links' do 
+  	test 'sidebar has all content & links' do 
   		assert page.has_content?('Website')
   		click_link('Blog')
   		click_link('About')
@@ -20,8 +19,6 @@ class PostsTest < ActionDispatch::IntegrationTest
   		assert page.has_link?('Instagram', href: 'http://instagram.com/josephmargolis')
   		assert page.has_link?('Github', href: 'https://github.com/storycoder')
   		assert page.has_link?('Email', href: 'mailto:joseph.margolis@gmail.com')
-
-#I need to find a DRY way to test for functional external links to urls and email.
 
   		assert page.has_content?('Admin Login')
   	end
@@ -37,11 +34,13 @@ class PostsTest < ActionDispatch::IntegrationTest
 		assert page.has_content?('Posts')
 
 		assert page.has_content?(@entry_one.title)
-		assert page.has_content?(@entry_one.body)
+		assert page.has_link?(@entry_one.title)
+		assert page.has_text?(@entry_one.body.length <= 250)
 
-		assert page.has_content?(@entry_two.title)
-		assert page.has_content?(@entry_two.body)
 
+		# assert (@entry_one.body.length <= 250 ) #I wrote this to test the function that limits no. of chars of per post seen by user on index to be no more that 250, but now I believe this test only provides length of entire text. NEEDS MORE WORK!!
+		
+		assert has_link?('READ MORE', @entry_one ) #This is edited to account for post_path being same as @entry_one in the fixture/setup - Still need a test for max # of characters. 
 	end
 
 	test 'show page shows post' do
